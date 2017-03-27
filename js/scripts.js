0b7582d99745a640abb766d94e41737fdb4c7742
Adam <slayer@plusnet.pl>
2017-03-27 22:26:25 +0200

    8.6 Funkcje dodane RETURN

--- js/scripts.js
+++ js/scripts.js
@@ -5,10 +5,10 @@ function getTriangleArea(a, h) {
        console.log('Nieprawidłowe dane');
     } else {
        console.log('Pole trójkąta to: ' + (a * h) / 2 );
+       return a*h/2;
     }
 }
 
 var triangle1Area = getTriangleArea(9, 16);
 var triangle2Area = getTriangleArea(1, 2);
 var triangle3Area = getTriangleArea(0, 6);
-//getTriangleArea(1, 6);
